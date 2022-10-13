7bc3bbdd1a4d789ea0aa8847406deac7a59ceb2a
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-13 12:34:01 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*main - entry point or main function
+*return -returns if successful
+*/
+
+int main(void)
+{	 
+/*string initialization*/
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return (0);
+}	
