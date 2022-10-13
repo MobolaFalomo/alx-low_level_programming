8ad2747976706db1a32a44f04d54116aae089040
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-13 14:20:01 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return - returns 0 if successful
- */
+  * main - Entry, printf function
+  * Return: Always 0
+  */
 int main(void)
-{		
-	printf("with proper grammar,but the outcome is a piece of art\n");	
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
 	return (0);
 }
+
