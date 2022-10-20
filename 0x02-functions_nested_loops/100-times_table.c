a6f0281e7dce5259958c0f1a0be367f3cefa046f
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-19 18:26:13 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,13 +25,15 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (prod < 10)
 					_putchar(' ');
+					_putchar(prod + '0');
 
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 100)) % 10 + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+					_putchar((prod % 10) + '0');
 				}
-				else if (prod < 100 && prod >= 0)
+				else if (prod < 100 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
