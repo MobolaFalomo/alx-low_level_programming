eaf4e1484c657122f561a02009610d6b6f0675f7
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-19 19:05:13 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,17 +12,18 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		for (mult = 0; mult <= 9; mult++)
-		{	
+		{
 			prod = num * mult;
 			if (num == 0)
 			{
 				_putchar(prod + '0');
 			}
-			
+
 			if (prod < 10 && num != 0)
 			{
 				_putchar(prod + '0');
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 			}
 			else if (prod >= 10)
