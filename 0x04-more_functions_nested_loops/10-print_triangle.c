19b811245857a124448e0b659a7f96de2953ff33
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-20 20:13:01 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,20 +1,33 @@
 #include "main.h"
 
 /**
-  * print_triangle - prints a triangle 
-  * @size: is the size of the triangle
-  *
-  * Return: return void
-  */
+ * print_triangle - prints a triangle
+ * @size: is the size of the triangle
+ *
+ * Return: return void
+ */
 void print_triangle(int size)
 {
 	if (size <= 0)
-		
+	{
 		_putchar('\n');
-	else int i, j;
-	for (i = 1; i <= size; i++)
-		for (j = i; j < size; j++)
-			_putchar(' ');
-	for (j = 1; j <= i; j++)
-		_putchar('#');
+	} else
+	{
+		int i, j;
 
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
