24be06a5512eb9154a2307607283742f82ce0dd8
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-20 19:54:13 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,18 +10,19 @@ void print_square(int size)
 {
 
 	if (size <= 0)
-		_putchar('\n');
-	
-	else
 	{
-	int i, j;
+		_putchar('\n');
+	} else
+	{
+		int i, j;
 
-	for (i = 0; i < size; i++)
-
-		for (j = 0; j < size; j++)
-			
-			_putchar('#');
-			
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
