da027ad7ab1506f582d8e4c9dbd48e901ca2a136
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-24 08:36:02 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse
+  * @s: a string
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i, length;
+
+	length = strlen(s);
+
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
