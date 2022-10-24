359f8cab91f8ed38999a51ae46e2460cd34d708c
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-24 07:35:15 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * swap_int - swaps the values of two integers
+  *@a: first pointer
+  *@b: second pointer
+  * Return: return void
+  */
+
+void swap_int(int *a, int *b)
+{
+	int constant = *a;
+	*a = *b;
+	*b = constant;
+
+}
+
