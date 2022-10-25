0ca1ecce362f19ecb585c7382739092d6819a62d
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-25 11:49:02 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	do {
-		i++;
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-	} while (src[i] != '\0');
+	dest[i] = '\0';
 
 	return (dest);
 }
