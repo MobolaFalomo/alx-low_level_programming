631f28db07418931b24dcb21a81acb8c25b451ff
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-27 14:11:43 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,11 @@ void reverse_array(int *a, int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; (i < (n - 1) / 2); i++)
 	{
-		n--;
 		j = a[i];
-		a[i] = a[n];
-		a[n] = j;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = j;
 	}
 	return (0);
 }
