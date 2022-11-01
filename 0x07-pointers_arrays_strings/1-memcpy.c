973348c3a3b5e15cfe12f74c672a70149391ecdc
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-31 18:38:32 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * *_memcpy - copies n bytes from memory area src to dest
+  * @src: memory area to copy from
+  * @dest: memory area to copy to
+  * @n: the number of bytes to copy from src
+  *
+  * Return: a pointer to dest
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int number = n;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
