0e2eac0d6877264d61ccdc4478535865a1c5080a
Mobola Falomo <mobolafalomo@gmail.com>
2022-10-31 20:20:12 -0700

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,9 +10,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; a[i][2]; i++)
+	for (i = 0; a[i][7]; i++)
 	{
-		for (j = 0; j < 2; j++)
+		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 
 		_putchar('\n');
