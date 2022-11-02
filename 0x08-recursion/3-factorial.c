38684c652de5ea49b8788f9c8641dbcc9d63a922
Mobola Falomo <mobolafalomo@gmail.com>
2022-11-02 15:15:00 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,8 +9,19 @@
 
 int factorial(int n)
 {
+	int a;
+
 	if (n < 0)
+	{
 		return (-1);
-	if (n == 0)
-		return (n * factorial(n - 1));
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		a = n * factorial(n - 1);
+	}
+	return (a);
 }
