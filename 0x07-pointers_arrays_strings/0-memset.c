2c23c3a03b659a50ff2b5a8fda5d0ee830da14ea
Mobola Falomo <mobolafalomo@gmail.com>
2022-11-02 17:05:13 -0700

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-  * *_memset - fills the first n bytes of the memory area pointed to by s with
-  * the constant byte b
+  * *_memset - fills the first n bytes of the memory area pointed
+  * to by s with the constant byte
   * @s - a pointer to the memory area to be filled
   * @b - the char to fill the memory area with
   * @n - number of bytes to be filled
@@ -10,16 +10,15 @@
   * Return: pointer to a memory area s
   */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(void *s, int b, size_t n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	unsigned int n = 0;
-	unsigned char *memory = s;
-	unsigned char value = b;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
 
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,8 +16,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned char *destination = dest;
 	const unsigned char *source = src;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
+	{
 		dest[i] = src[i];
-
+		i++;
+	}
 	return (dest);
 }
