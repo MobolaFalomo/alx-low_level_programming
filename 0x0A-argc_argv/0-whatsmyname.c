122dcc373539c87ca3e1db447a51348bbd90d8fb
Mobola Falomo <mobolafalomo@gmail.com>
2022-11-04 14:49:14 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - program prints its name
+  * @argc: counts argument
+  * @argv: array for all the argument
+  * Return: return 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
